854ff376d8fae086c16d86ddead63c888f967643
bella.tassone <itassone@wesleyan.edu>
2023-10-02 15:27:12 -0400

    Fixing styling of button #25

--- landing.js
+++ landing.js
@@ -1,8 +1,8 @@
 function showNavItems() {
     var x = document.getElementById("navbar_list");
-    if (x.style.display === "none") {
-      x.style.display = "flex";
-    } else {
+    if (x.style.display === "flex") {
       x.style.display = "none";
+    } else {
+      x.style.display = "flex";
     }
   }
