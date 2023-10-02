d89a8f175f78122b711dcc3f7572c5fc88fb0175
bella.tassone <itassone@wesleyan.edu>
2023-10-02 14:55:56 -0400

    Adding button tag #25

---
+++ landing.js
@@ -0,0 +1,8 @@
+function showNavItems() {
+    var x = document.getElementById("navbar_list");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
