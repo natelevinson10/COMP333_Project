6c91b46f656fa600e29275637583cfa1a5818eb8
Nate Levinson <natelevinson10@gmail.com>
2023-10-13 14:15:47 -0400

    remove top php tags (issue #46)

--- ratings.php
+++ ratings.php
@@ -8,58 +8,6 @@
     <link rel="icon" href= "public/music.png" type="image/x-icon">
     <script src="https://kit.fontawesome.com/289e976bd2.js" crossorigin="anonymous"></script>
 </head>
-
-<body>
-     <?php
-        //display errors (for debugging)
-        //error_reporting(E_ALL);
-        //ini_set('display_errors', '1');
-        $servername = "localhost";
-        $username = "root";
-        $password = "";
-        $dbname = "music_db";
-        $conn = new mysqli($servername, $username, $password, $dbname);
-        if ($conn->connect_error) {
-        die("Connection failed: " . $conn->connect_error);
-        }
-        if(isset($_REQUEST["submit"])){
-            $out_value = "";
-            $user = $_REQUEST['userid'];
-            $pass = $_REQUEST['login_password_1'];
-
-            //prepared statement
-            $sql_query = "SELECT password FROM users WHERE username = ?";
-            $stmt = mysqli_prepare($conn, $sql_query);
-            mysqli_stmt_bind_param($stmt, "s", $user);
-            mysqli_stmt_execute($stmt);
-            $result = mysqli_stmt_get_result($stmt);
-            $num = mysqli_num_rows($result);
-
-            //check if a matching username exists
-            if ($num > 0) {
-                //if yes, retrieve stored hashed password
-                //this works because there should only ever be one row since usernames are unique
-                $row = mysqli_fetch_assoc($result);
-                $hashed_pass = $row["password"];
-
-                //check if the user-provided password matches the stored hashed password
-                $password_match = password_verify($pass, $hashed_pass);
-                if ($password_match) {
-                    //success!!
-                    //set session variables
-                    $_SESSION["loggedin"] = true;
-                    $_SESSION["username"] = $user;
-                    $_SESSION["password"] = $hashed_pass;
-                    //redirect (will eventually be ratings page)
-                    header('Location: index.html');
-                }
-            }
-            //if either if statement returns false, then something was incorrect
-            $out_value = "Your username or password was incorrect!";
-        }
-        //close connection
-        $conn->close();
-    ?>
     <!-- Navigation Bar -->
     <div id="navbar" class="row navbar">
         <div class="navbar_logo" style= "padding-top:20px;">
